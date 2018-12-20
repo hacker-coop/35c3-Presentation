bf1ec395e9e8413c0e40fc03c46fdf905900ecd3
Anton Mueller <anmueller@b1-systems.de>
2018-12-20 13:38:59 +0100

    added MindMap of @ajuvo

---
+++ images/hacker eg vortrag v1.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="hacker eg vortrag v1" FOLDED="false" ID="ID_1182348975" CREATED="1545174759628" MODIFIED="1545174776999" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="was" POSITION="right" ID="ID_637384518" CREATED="1545176871163" MODIFIED="1545176873480">
+<edge COLOR="#ff00ff"/>
+<node TEXT="einleitung" ID="ID_823568589" CREATED="1545174936600" MODIFIED="1545176884530">
+<node TEXT="mehr grunds&#xe4;tzlich" ID="ID_1819032186" CREATED="1545174940637" MODIFIED="1545174943588"/>
+<node TEXT="verweis auf videos, folien, webseite, htaccess" ID="ID_1214632985" CREATED="1545174943839" MODIFIED="1545174959827"/>
+<node TEXT="kurzer praktischer abri&#xdf; zum schlu&#xdf;" ID="ID_1212992359" CREATED="1545176447252" MODIFIED="1545176455654"/>
+</node>
+<node TEXT="warum" ID="ID_1106355646" CREATED="1545176461625" MODIFIED="1545176897905">
+<node TEXT="was ist eine genossenschaft" ID="ID_1279990937" CREATED="1545176468857" MODIFIED="1545176480961"/>
+<node TEXT="f&#xf6;rdergen&apos;t" ID="ID_1969368673" CREATED="1545176522437" MODIFIED="1545176528778"/>
+<node TEXT="produktivgen&apos;t" ID="ID_1952621167" CREATED="1545176529192" MODIFIED="1545176533383"/>
+<node TEXT="grunds&#xe4;tzliches" ID="ID_1927294092" CREATED="1545176723321" MODIFIED="1545176726962">
+<node TEXT="problematisierung" ID="ID_1568201902" CREATED="1545176730640" MODIFIED="1545176734939"/>
+<node TEXT="l&#xf6;sung" ID="ID_1129626850" CREATED="1545176736055" MODIFIED="1545176738172"/>
+</node>
+<node TEXT="praktische usecases" ID="ID_317802949" CREATED="1545176547641" MODIFIED="1545176552329">
+<node TEXT="nebeneinnahmen" ID="ID_1520220502" CREATED="1545176553115" MODIFIED="1545176561981"/>
+<node TEXT="unternehmen zum ein- und austreten" ID="ID_743441601" CREATED="1545176564415" MODIFIED="1545176572309"/>
+<node TEXT="spendensammler" ID="ID_967303356" CREATED="1545176577578" MODIFIED="1545176594081"/>
+<node TEXT="crowdfunding" ID="ID_113927998" CREATED="1545176594680" MODIFIED="1545176598466"/>
+<node TEXT="bezahlplattform" ID="ID_28805846" CREATED="1545176598858" MODIFIED="1545176602932"/>
+<node TEXT="dienstleister" ID="ID_1502043872" CREATED="1545176607541" MODIFIED="1545176613495"/>
+<node TEXT="lobbyismus, aktivismus" ID="ID_101063816" CREATED="1545176620742" MODIFIED="1545176632157"/>
+<node TEXT="rechtsberatung, steuerberatung" ID="ID_1991626393" CREATED="1545176632613" MODIFIED="1545176642244"/>
+<node TEXT="rechtsschutz" ID="ID_1151835309" CREATED="1545176642784" MODIFIED="1545176645695"/>
+</node>
+</node>
+<node TEXT="schluss" ID="ID_1331766447" CREATED="1545176747154" MODIFIED="1545176903926">
+<node TEXT="praktisches" ID="ID_1472312196" CREATED="1545176751325" MODIFIED="1545176753489">
+<node TEXT="gr&#xfc;ndungsprozess" ID="ID_474922256" CREATED="1545176753843" MODIFIED="1545176759256"/>
+<node TEXT="n&#xe4;chste schritte" ID="ID_1394016405" CREATED="1545176767963" MODIFIED="1545176772054"/>
+</node>
+<node TEXT="aufruf" ID="ID_1202011097" CREATED="1545176776674" MODIFIED="1545176780711"/>
+</node>
+</node>
+<node TEXT="wie" POSITION="left" ID="ID_1241989168" CREATED="1545176910582" MODIFIED="1545176913797">
+<edge COLOR="#00ffff"/>
+<node TEXT="schwerpunkt beim grunds&#xe4;tzlichen" ID="ID_1172540220" CREATED="1545176914777" MODIFIED="1545176924624"/>
+<node TEXT="usecases nur anrei&#xdf;en" ID="ID_1415394838" CREATED="1545176925004" MODIFIED="1545176931306">
+<node TEXT="ansprechende beispiele" ID="ID_1196776932" CREATED="1545176931604" MODIFIED="1545176939795"/>
+</node>
+<node TEXT="schlu&#xdf;punkt: aufruf!" ID="ID_757515034" CREATED="1545177253350" MODIFIED="1545177263784"/>
+</node>
+</node>
+</map>
